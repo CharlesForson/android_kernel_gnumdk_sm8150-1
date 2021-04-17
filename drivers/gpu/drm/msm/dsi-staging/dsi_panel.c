08ab1a4fd5be321bf87403593171bf2a44dfe835
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2021-04-18 00:08:34 +0200

    Enable more power saver on display

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -2568,17 +2568,9 @@ static int dsi_panel_parse_misc_features(struct dsi_panel *panel)
 {
 	struct dsi_parser_utils *utils = &panel->utils;
 
-	panel->ulps_feature_enabled =
-		utils->read_bool(utils->data, "qcom,ulps-enabled");
+	panel->ulps_feature_enabled = true;
 
-	pr_info("%s: ulps feature %s\n", __func__,
-		(panel->ulps_feature_enabled ? "enabled" : "disabled"));
-
-	panel->ulps_suspend_enabled =
-		utils->read_bool(utils->data, "qcom,suspend-ulps-enabled");
-
-	pr_info("%s: ulps during suspend feature %s", __func__,
-		(panel->ulps_suspend_enabled ? "enabled" : "disabled"));
+	panel->ulps_suspend_enabled = true;
 
 	panel->te_using_watchdog_timer = utils->read_bool(utils->data,
 					"qcom,mdss-dsi-te-using-wd");
@@ -2586,8 +2578,9 @@ static int dsi_panel_parse_misc_features(struct dsi_panel *panel)
 	panel->sync_broadcast_en = utils->read_bool(utils->data,
 			"qcom,cmd-sync-wait-broadcast");
 
-	panel->lp11_init = utils->read_bool(utils->data,
-			"qcom,mdss-dsi-lp11-init");
+    // Disabled for allow-phy-poweroff 
+	panel->lp11_init = false;
+
 	return 0;
 }
 
